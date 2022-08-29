eb3b93cafd01d13a16e613704786e8c1cef30438
OveFrode <111500112+OveFrode@users.noreply.github.com>
2022-08-29 14:28:41 +1000

    Menu Update

--- Assets/UI/Swipe_menu.cs
+++ Assets/UI/Swipe_menu.cs
@@ -43,7 +43,20 @@ public class Swipe_menu : MonoBehaviour
             }
         }
         
-  
+  for (int i = 0; i < pos.Length; i++)
+        {
+            if(scroll_pos < pos[i] + (distance / 2) && scroll_pos > pos[i] - (distance/2))
+            {
+                transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1.5f, 1.5f), 0.1f);
+            for(int a = 0; a < pos.Length; a++)
+                {
+                    if(a != i)
+                    {
+                        transform.GetChild(a).localScale = Vector2.Lerp(transform.GetChild(a).localScale, new Vector2(0.8f, 0.8f), 0.1f);
+                    }
+                }
+            }
+        }
           
                 
             }
