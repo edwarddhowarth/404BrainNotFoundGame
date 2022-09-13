0815670f614b65b332a658660dc31f6aebfb5af4
OveFrode <111500112+OveFrode@users.noreply.github.com>
2022-09-13 14:54:49 +1000

    Dialogue text and animations
    This allows for adding sentences and loops through them the text box will loop for now

---
+++ Assets/DialogueController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class DialogueController : MonoBehaviour
+{
+    public TextMeshProUGUI DialogueText;
+    public float DialogueSpeed;
+    public Animator DialogueAnimator;
+    public string[] Sentences;
+    private int Index = 0;
+    private bool StartDialogue = true;
+
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        //Input for next can be changed
+
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            if (StartDialogue)
+            {
+                DialogueAnimator.SetTrigger("Enter");
+                StartDialogue = false;
+            }
+            else
+            {
+                NextSentence();
+            }
+        }
+
+
+
+    }
+
+
+    void NextSentence()
+    {
+        if(Index <= Sentences.Length - 1)
+        {
+            DialogueText.text = "";
+            StartCoroutine(WriteSentence());
+        }
+        else
+        {
+            DialogueText.text = "";
+            DialogueAnimator.SetTrigger("Exit");
+            Index = 0;
+            StartDialogue = true;
+        }
+    }
+
+    IEnumerator WriteSentence()
+    {
+        foreach(char Character in Sentences[Index].ToCharArray())
+        {
+            DialogueText.text += Character;
+            yield return new WaitForSeconds(DialogueSpeed);
+        }
+        Index++;
+    }
+}
