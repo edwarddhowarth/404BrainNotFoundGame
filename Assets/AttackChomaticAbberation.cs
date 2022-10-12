69cc229a20a44ab05d504c1bef02fcc132a01fa0
edwarddhowarth <edwarddhowarth@gmail.com>
2022-10-12 16:37:05 +1100

    CA PP on hit

--- Assets/AttackChomaticAbberation.cs
+++ Assets/AttackChomaticAbberation.cs
@@ -1,23 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
 
 public class AttackChomaticAbberation : MonoBehaviour
 {
+    GameObject camera;
+    PostProcessVolume vol;
+    ChromaticAberration CA;
+    float startingCAValue;
+    float CACounter;
+    bool hit;
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = transform.parent.gameObject;
+        vol = camera.GetComponent<PostProcessVolume>();
+        vol.profile.TryGetSettings( out CA);
+        startingCAValue = CA.intensity.value;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(hit)
+        {
+            CA.intensity.value = CA.intensity.value - Time.deltaTime * 2f;
+
+            if(CA.intensity.value <= startingCAValue)
+            {
+                hit = false;
+                CA.intensity.value = startingCAValue;
+            }
+        }
         
     }
 
     public void onHitBullet()
     {
-
+        CA.intensity.value = 1f;
+        hit = true;
     }
 }
