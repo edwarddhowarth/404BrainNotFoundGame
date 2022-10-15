3414d4cfd97363e4085b52bf364e300053e4b7c0
edwarddhowarth <edwarddhowarth@gmail.com>
2022-10-16 04:20:10 +1100

    level 4 ai and removed core door

--- Assets/AttackMusic.cs
+++ Assets/AttackMusic.cs
@@ -12,6 +12,8 @@ public class AttackMusic : MonoBehaviour
     bool wasEngaged;
     float countdown = 0f;
     float escapeCooldown = 0f;
+    bool finalHack = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,64 +24,73 @@ public class AttackMusic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // IF player is not engaged, and hasn't recently been in combat and countdown, do nothing
-        if (!playerEngaged && !wasEngaged)
+        if (!finalHack)
         {
-            if(countdown <= 0f)
+            // IF player is not engaged, and hasn't recently been in combat and countdown, do nothing
+            if (!playerEngaged && !wasEngaged)
             {
-                countdown = 0f;
+                if (countdown <= 0f)
+                {
+                    countdown = 0f;
+                }
+
             }
+            else if (playerEngaged && !wasEngaged) // If the player hasn't just been in combat and has just been engaged
+            {
+
+
+
+
+                if (escapeCooldown <= 0f)
+                {
+                    Debug.Log("Player Engaged for first time");
+                    countdown = 1f;
+                    source.loop = true;
+                    source.clip = main;
+                    source.Play();
+                    wasEngaged = true;
+                }
+                else if (escapeCooldown >= -.1f)
+                {
+                    escapeCooldown -= Time.deltaTime;
+                }
+
+
+            }
+            // player no longer being engaged but was just in combat and still in countdown
+            else if (!playerEngaged && wasEngaged)
+            {
+                if (countdown > -.2f)
+                {
+                    countdown -= Time.deltaTime;
+                }
+
+                // Debug.Log("calc: " + (source.timeSamples / 44100.0f));
+
+                if (countdown <= 0f && ((source.timeSamples / 44100.0f) % 3.30f < .01f))
+                {
+                    Debug.Log("Stopping music");
+                    wasEngaged = false;
+                    source.Stop();
+                    source.clip = tail;
+                    source.loop = false;
+                    source.Play();
+
+
+                    escapeCooldown = 1f;
+                }
+            }
+
+
+            playerEngaged = false;
+
+
 
         }
-        else if (playerEngaged && !wasEngaged) // If the player hasn't just been in combat and has just been engaged
+        else
         {
-            
-            
-            
-
-            if(escapeCooldown <= 0f)
-            {
-                Debug.Log("Player Engaged for first time");
-                countdown = 1f;
-                source.loop = true;
-                source.clip = main;
-                source.Play();
-                wasEngaged = true;
-            }
-            else if(escapeCooldown >= -.1f)
-            {
-                escapeCooldown -= Time.deltaTime;
-            }
-
-
+            source.Stop();
         }
-        // player no longer being engaged but was just in combat and still in countdown
-        else if (!playerEngaged && wasEngaged) 
-        {
-            if (countdown > -.2f)
-            {
-                countdown -= Time.deltaTime;
-            }
-
-           // Debug.Log("calc: " + (source.timeSamples / 44100.0f));
-
-            if(countdown <= 0f && ((source.timeSamples / 44100.0f) % 3.30f < .01f))
-            {
-                Debug.Log("Stopping music");
-                wasEngaged = false;
-                source.Stop();
-                source.clip = tail;
-                source.loop = false;
-                source.Play();
-                
-
-                escapeCooldown = 1f;
-            }
-        }
-
-
-        playerEngaged = false;
-
 
 
 
@@ -93,6 +104,11 @@ public class AttackMusic : MonoBehaviour
             playerEngaged = true;
         }
 
+    }
+
+    public void EndMusic()
+    {
+        finalHack = true;
     }
 
 }
